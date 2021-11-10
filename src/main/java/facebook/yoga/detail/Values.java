e822b663cb62380c84a2da75577a1bf324baf57e
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2021-11-10 00:05:08 +0000

    We do a little Values

--- src/main/java/facebook/yoga/detail/Values.java
+++ src/main/java/facebook/yoga/detail/Values.java
@@ -1,83 +1,51 @@
 package facebook.yoga.detail;
 
-import java.util.*;
+import facebook.yoga.YGValue;
+import java.util.ArrayList;
 
-//C++ TO JAVA CONVERTER TODO TASK: Most C++ 'constraints' are not converted by C++ to Java Converter:
-//ORIGINAL LINE: template <size_t Size>
-//C++ TO JAVA CONVERTER WARNING: The original C++ template specifier was replaced with a Java generic specifier, which may not produce the same behavior:
-//ORIGINAL LINE: template <typename Size> requires size_t<Size>
-public class Values<Size> //Type originates from: Yoga-internal.h
+
+public class Values //Type originates from: Yoga-internal.h
 {
-  private ArrayList<CompactValue> values_ = new ArrayList<>(Size);
+    private ArrayList<CompactValue> values_;
+    private final int size;
 
-//C++ TO JAVA CONVERTER TODO TASK: Java has no equivalent to ' = default':
-//  Values() = default;
-  public Values(final YGValue defaultValue)
-  {
-	values_.fill(defaultValue);
-  }
 
-  public final CompactValue get(Integer i)
-  {
-//C++ TO JAVA CONVERTER TODO TASK: The following line was determined to contain a copy constructor call - this should be verified and a copy constructor should be created:
-//ORIGINAL LINE: return values_[i];
-	  return CompactValue.createCompactValue(values_.get(i));
-  }
-  public final void set(Integer i, CompactValue value)
-  {
-	  values_.set(i, value);
-  }
-  public final CompactValue get(Integer i)
-  {
-//C++ TO JAVA CONVERTER TODO TASK: The following line was determined to contain a copy constructor call - this should be verified and a copy constructor should be created:
-//ORIGINAL LINE: return values_[i];
-	  return CompactValue.createCompactValue(values_.get(i));
-  }
-  public final void set(Integer i, CompactValue value)
-  {
-	  values_.set(i, value);
-  }
+    //  Values() = default;
+    public Values(final YGValue defaultValue, int size) {
+        values_ = new ArrayList<>(size);
+        this.size = size;
+        values_.add(CompactValue.createCompactValue(defaultValue));
+    }
 
-//C++ TO JAVA CONVERTER TODO TASK: Most C++ 'constraints' are not converted by C++ to Java Converter:
-//ORIGINAL LINE: template <size_t I>
-//C++ TO JAVA CONVERTER WARNING: The original C++ template specifier was replaced with a Java generic specifier, which may not produce the same behavior:
-//ORIGINAL LINE: template <typename I> requires size_t<I>
-  public final <I> YGValue get()
-  {
-	return std::get<I>(values_);
-  }
+    public final CompactValue get(Integer i) {
+        return CompactValue.createCompactValue(values_.get(i).convertToYgValue());
+    }
 
-//C++ TO JAVA CONVERTER TODO TASK: Most C++ 'constraints' are not converted by C++ to Java Converter:
-//ORIGINAL LINE: template <size_t I>
-//C++ TO JAVA CONVERTER WARNING: The original C++ template specifier was replaced with a Java generic specifier, which may not produce the same behavior:
-//ORIGINAL LINE: template <typename I> requires size_t<I>
-  public final <I> void set(YGValue value)
-  {
-	std::get<I>(values_) = value;
-  }
+    public final void set(Integer i, CompactValue value) {
+        values_.set(i, value);
+    }
 
-//C++ TO JAVA CONVERTER TODO TASK: Most C++ 'constraints' are not converted by C++ to Java Converter:
-//ORIGINAL LINE: template <size_t I>
-//C++ TO JAVA CONVERTER WARNING: The original C++ template specifier was replaced with a Java generic specifier, which may not produce the same behavior:
-//ORIGINAL LINE: template <typename I> requires size_t<I>
-//C++ TO JAVA CONVERTER TODO TASK: 'rvalue references' have no equivalent in Java:
-  public final <I> void set(YGValue && value)
-  {
-	this.<I>set(value);
-  }
+    public final CompactValue getCompactValue(Integer i) {
+        return CompactValue.createCompactValue(values_.get(i).convertToYgValue());
+    }
 
-  public boolean equalsTo(final Values other)
-  {
-	for (Integer i = 0; i < Size; ++i)
-	{
-	  if (values_.get(i) != other.values_.get(i))
-	  {
-		return false;
-	  }
-	}
-	return true;
-  }
 
-//C++ TO JAVA CONVERTER TODO TASK: Java has no equivalent to ' = default':
-//  Values& operator =(const Values& other) = default;
+
+    public final YGValue get(int i) {
+        return values_.get(i).convertToYgValue();
+    }
+
+
+    public final void set(YGValue value, int i) {
+        values_.set(i, CompactValue.createCompactValue(value));
+    }
+
+    public boolean equalsTo(final Values other) {
+        for (int i = 0; i < size; ++i) {
+            if (values_.get(i) != other.values_.get(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
