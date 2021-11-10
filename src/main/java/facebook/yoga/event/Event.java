3c4b09dcb874ed5a499ff2cbe0184c0c7b8417a2
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2021-11-10 01:13:14 +0000

    We stay hungry, we devour

--- src/main/java/facebook/yoga/event/Event.java
+++ src/main/java/facebook/yoga/event/Event.java
@@ -1,57 +1,46 @@
 package facebook.yoga.event;
 
 import facebook.yoga.YGNode;
-import org.w3c.dom.Node;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.function.Consumer;
 
 public class Event //Type originates from: event.h
 {
+    private static HashMap<Class<?>, Collection<Consumer<CallableEvent>>> listeners;
+
     public static void reset() //Method definition originates from: event.cpp
     {
-        var head = facebook.yoga.event.GlobalMembers.push(null);
-        while (head != null) {
-            var current = new auto(head);
-            head = head.next;
-            current = null;
+        listeners.clear();
+    }
+
+
+    public static <T extends CallableEvent> void subscribe(Class<T> clazz, Consumer<T> listener) //Method definition originates from: event.cpp
+    {
+        listeners.computeIfAbsent(clazz, aClass -> new ConcurrentLinkedDeque<>()).add(
+                (Consumer<CallableEvent>) listener);
+    }
+
+
+    public static void publish(final YGNode node) {
+        publish(node, new EmptyEventData());
+    }
+
+
+    public static void publish(final YGNode node, final CallableEvent eventData) {
+        Collection<Consumer<CallableEvent>> listeners = Event.listeners.get(eventData.getClass());
+        if (listeners != null) {
+            for (Consumer<CallableEvent> listener : listeners) {
+                listener.accept(eventData);
+            }
         }
     }
 
-//  class Data;
 
-//  typedef void(final YGNode UnnamedParameter, Type UnnamedParameter2, DataSubscriber UnnamedParameter3);
-
-
-    public static void subscribe(std::function<Subscriber>&&subscriber) //Method definition originates from: event.cpp
+    private static void publish(final YGNode node, Type eventType, final CallableEvent eventData) //Method definition originates from: event.cpp
     {
-        facebook.yoga.event.GlobalMembers.push(new Node(new std::function (std::move (subscriber))));
-    }
-
-
-    public static <E> void publish(final YGNode node) {
-        publish(node, new EmptyEventData<E>());
-    }
-
-
-    public static <E> void publish(final YGNode node, final CallableEvent eventData) {
-
-
-    }
-
-
-    public static <E> void publish(YGNode node) {
-        publish(node, new EmptyEventData<E>());
-    }
-
-
-    public static <E> void publish(YGNode node, final EmptyEventData<E> eventData) {
-        facebook.yoga.Event.<E>publish(node, eventData);
-    }
-
-    private static void publish(final YGNode node, Type eventType, final Data eventData) //Method definition originates from: event.cpp
-    {
-        for (var subscriber = GlobalMembers.subscribers.load(
-                std::memory_order_relaxed); subscriber != null; subscriber = subscriber.next) {
-            subscriber.subscriber(node, eventType, eventData);
-        }
+        publish(node, eventData);
     }
 
     public enum Type //Type originates from: event.h
@@ -78,22 +67,8 @@ public class Event //Type originates from: event.h
     }
 
 
-    public static class EmptyEventData<E> extends CallableEvent  //Type originates from: event.h
+    public static class EmptyEventData extends CallableEvent  //Type originates from: event.h
     {
     }
 
-    public static class Data<E> //Type originates from: event.h
-    {
-        private final Object data_;
-
-
-        public Data(final EmptyEventData<E> data) {
-            this.data_ = data;
-        }
-
-
-        public final <E> EmptyEventData<E> get() {
-            return (EmptyEventData<E>) data_;
-        }
-    }
 }
