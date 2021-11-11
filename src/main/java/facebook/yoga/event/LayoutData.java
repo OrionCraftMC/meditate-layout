2f72b451f27084ad428d2f2ac7175e451181c139
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2021-11-11 17:11:22 +0000

    OOB fix

--- src/main/java/facebook/yoga/event/LayoutData.java
+++ src/main/java/facebook/yoga/event/LayoutData.java
@@ -11,5 +11,11 @@ public class LayoutData extends CallableEvent //Type originates from: event.h
     public int cachedLayouts;
     public int cachedMeasures;
     public int measureCallbacks;
-    public @NotNull ArrayList<Integer> measureCallbackReasonsCount = new ArrayList<>(LayoutPassReason.COUNT.getValue());
+    public @NotNull ArrayList<Integer> measureCallbackReasonsCount = new ArrayList<>();
+
+    public LayoutData() {
+        for (int i = 0; i < LayoutPassReason.COUNT.getValue(); i++) {
+            measureCallbackReasonsCount.add(0);
+        }
+    }
 }
