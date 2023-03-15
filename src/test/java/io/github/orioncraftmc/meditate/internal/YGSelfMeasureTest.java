77b8ba7c3635ef16822b73782a53ed6ff5b35305
Chris Hansen <chansen@webfoot-designs.com>
2023-03-15 13:37:00 -0500

    Fix recursion in test

--- src/test/java/io/github/orioncraftmc/meditate/internal/YGSelfMeasureTest.java
+++ src/test/java/io/github/orioncraftmc/meditate/internal/YGSelfMeasureTest.java
@@ -5,7 +5,12 @@ import io.github.orioncraftmc.meditate.enums.YogaMeasureMode;
 
 public class YGSelfMeasureTest {
     public static YGSize measure(YogaNode node, float width, YogaMeasureMode widthMode, float height, YogaMeasureMode heightMode) {
-        var measuredWidth = node.measure(width, widthMode.intValue(), 0f, YogaMeasureMode.EXACTLY.intValue()).width;
+        var measuredWidth = width;
+
+        if(measuredWidth == 0f || measuredWidth == Float.NaN) {
+            measuredWidth = node.measure(width, widthMode.intValue(), 0f, YogaMeasureMode.EXACTLY.intValue()).width;
+        }
+
         var newHeight = measuredWidth * .75f;
         return new YGSize(measuredWidth, newHeight);
     }
